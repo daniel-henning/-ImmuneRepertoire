551e5aeca9514013350ab7d92287368d5a80666c
ninghe <291784082@qq.com>
2024-03-29 21:21:12 +0800

    Update TRUST4.sh

--- TRUST4.sh
+++ TRUST4.sh
@@ -1,4 +1,4 @@
-
+# for bulk TCR/BCR analysis.
 run-trust4 \
 -1 /media/ning/backup/TCR/fq/GSE210337/SRR20751886_1.fastq.gz \
 -2 /media/ning/backup/TCR/fq/GSE210337/SRR20751886_2.fastq.gz \
@@ -8,3 +8,19 @@ run-trust4 \
 --repseq \
 -o /data/work/TCR-Qiao/test/SRR20751886
 
+
+# for single cell TCR/BCR analysis.
+run-trust4 \
+-1 $fq1 \
+-2 $fq2 \
+-f /data/Apps/TRUST4/hg38_bcrtcr.fa \
+--ref /data/Apps/TRUST4/human_IMGT+C.fa \
+--read1Range 28 -1 \
+--read2Range 0 -1 \
+--barcode $fq1 \
+--barcodeRange 0 15 + \
+--UMI $fq1 \
+--umiRange 16 27 + \
+--repseq \
+--outputReadAssignment \
+-o $output
